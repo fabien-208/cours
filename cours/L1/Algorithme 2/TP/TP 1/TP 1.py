d7acd2a1132e48ed4734d185ec0fd2ce7e16924e
fabien <fabien.2084@gmail.com>
2024-01-24 14:13:23 +0100

    maj

--- cours/L1/Algorithme 2/TP/TP 1/TP 1.py
+++ cours/L1/Algorithme 2/TP/TP 1/TP 1.py
@@ -26,3 +26,86 @@ def max_distance(figure:list[tuple[int]]) -> float:
     
     
 # Exercice 2
+
+
+
+livre1 = {"Auteur": "Sartre", "Annee":19964, "Titre": "Les mots","Prix":13.5, "Quantité":20}
+livre2 = {"Auteur": "Simon Veil", "Annee":2007, "Titre":"Une vie", "Prix":15, "Quantité":35}
+livre3 = {"Auteur": "Rousseau", "Annee":1770, "Titre":"Les confessions", "Prix":13.5, "Quantité":45}
+livre4 = {"Auteur": "Zola", "Annee":1861, "Titre":"Perrette", "Prix":11.5, "Quantité":51}
+livre5 = {"Auteur": "De Musset", "Annee":1849, "Titre":"Louison", "Prix":11, "Quantité":34}
+
+catalogue = {"Autobiographie":[livre2, livre3], "Roman":[livre1],"Pièces de théâtre":[livre4, livre5]}
+
+def ajoute_livre(auteur, annee, titre, quantité, genre, catalogue):
+    if genre not in catalogue:
+        catalogue[genre] = []
+    return catalogue[genre].append({"Auteur": auteur, "Annee": annee, "Titre":titre, "Quantité": quantité})
+
+
+def estpresent(catalogue, titre):
+    for genre in catalogue:
+        for livre in genre:
+            if titre == livre['Titre']:
+                return True
+    return False 
+
+
+def affiche_livre(livre):
+    print("'Auteur': {}".format(livre['Auteur']) + '\n' + "'Titre': {}".format(livre['Titre'])+ '\n' + "'Titre': {}".format(livre['Titre'])+ '\n' + "'Prix': {}".format(livre['Prix'])+ '\n' + "'Quantité': {}".format(livre['Quantité']))
+
+
+
+def changer_prix(livre , prix):
+    livre['Prix'] = prix
+    
+    
+def ajoute_quantité(catalogue, titre, quantité):
+    for genre in catalogue:
+        for livre in genre:
+            if livre['Titre'] == titre:
+                livre['Quantité'] += quantité
+                
+                
+                
+def livre_auteur(nom_auteur, catalogue):
+    liste_livre = []
+    for genre in catalogue:
+        for livre in genre:
+            if livre['Auteur'] == nom_auteur:
+                liste_livre.append(livre['Titre'])
+    return liste_livre
+
+
+
+
+def livre_anne(catalogue, date):
+    liste_livre = []
+    for genre in catalogue:
+        for livre in genre:
+            if livre['Annee'] == date:
+                liste_livre.append(livre['Titre'])
+    return liste_livre
+
+
+def les_plus_chers(catalogue):
+    liste = []    
+    for genre in catalogue:
+        max = 0
+        for livre in genre:
+            if livre['Prix'] > max:
+                max = livre['Prix']
+                livr = livre['Titre']
+        liste.append(livr)
+    return liste
+
+
+
+def genre_littéraire(auteur, catalogue):
+    liste = []
+    for genre in catalogue:
+        for livre in genre:
+            if livre['Auteur'] == auteur:
+                liste.append(genre)
+    return liste
+        
---
+++ cours/L1/Algorithme 2/TP/TP 2/TP 2.py
@@ -0,0 +1,120 @@
+
+# Exercice 1
+
+class compte_bancaire:
+    
+    def __init__(self, nom, solde=1000):
+        """
+        >>> compte1 = compte_bancaire('Michu', 800)
+        >>> compte1.depot(350)
+        >>> compte1.retrait(200)
+        >>> compte1.affiche()
+        le solde du compte bancaire de Michu est de 950 euros
+        """
+        self.nom = nom
+        self.solde = solde
+        
+        
+        
+    def depot(self, somme):
+        self.solde += somme
+        
+    
+    def retrait(self, somme):
+        self.solde -= somme
+        
+    def affiche(self):
+        print('le solde du compte bancaire de {} est de {} euros'.format(self.nom, self.solde))
+        
+        
+# Exercice 2
+
+class voiture:
+    def __init__(self, nom, couleur, vitesse=0, tu=None):
+        """
+        >>> voitu = voiture('ede', 'rouge')
+        >>> voitu.accelere(60)
+        >>> print(voitu.vitesse)
+        60
+        >>> voitu.freine(49)
+        >>> print(voitu.vitesse)
+        11
+        >>> print(voitu.str())
+        ['ede', 'rouge', 11]
+        >>> voitu.affiche()
+        la voiture s'apelle ede, elle est de couleur rouge et sa vitesse est de 11 km/h
+        """
+        self.nom = nom
+        self.couleur = couleur
+        self.vitesse= vitesse
+        self.tu = tu
+        
+    
+    def accelere(self, inc):
+        while inc >= 10:
+            if self.vitesse < 130:
+                self.vitesse += 10
+                inc -= 10
+            else:
+                break
+        if self.vitesse < 130:
+            self.vitesse += inc
+        
+            
+            
+            
+    def freine(self, dec):
+        if dec >= self.vitesse:
+            self.vitesse = 0
+        else:
+            self.vitesse -= dec
+        
+        
+    def str(self):
+        self.tu=[self.nom, self.couleur, self.vitesse]
+        return self.tu
+        
+        
+        
+    def affiche(self):
+        print("la voiture s'apelle {}, elle est de couleur {} et sa vitesse est de {} km/h".format(self.nom, self.couleur, self.vitesse))
+    
+
+                
+# Exercice 3
+
+# Question 1
+
+import random 
+
+class Carte:
+    def __init__(self):
+        """
+        >>> ch = Carte
+        >>> ch.affiche()
+        """
+        
+        coul = ['trèfle', 'carreau', 'coeur', 'pique']
+        haut = [7,8,9,'Valet','Dame', 'Roi', 10, 'As']
+        point = [0, 0, 0, 2, 3, 4, 10, 11]
+        atout = [7, 8, 'Dame', 'Roi', 10, 'As', 9, 'Valet']
+        point_atout = [0, 0, 0, 2, 3, 4, 14, 20]
+        ch_coul = random.randint(len(coul))
+        ch_haut = random.randint(len(hait))
+        
+        self.couleur = coul[ch_coul]
+        self.hauteur = haut[ch_haut]
+        self.nb_point = point[ch_haut]
+        
+        
+    def affiche(self):
+        print('{} de {}'.format(self.hauteur, self.couleur))
+
+    def cou():
+        pass
+                
+    
+if '__main__' ==__name__:
+    import doctest
+    doctest.testmod(verbose=True)
+                

15aba7107234bfc03e9e834ad5b914ff1150835b
fabien <fabien.2084@gmail.com>
2024-02-23 12:46:35 +0100

    fvf

--- cours/L1/Algorithme 2/TD/TD 4.py
+++ cours/L1/Algorithme 2/TD/TD 4.py
@@ -3,12 +3,23 @@ class matrice:
 
     def __init__(self, mat:list[list[int]]) -> None:
         """
-        >>> bfrh = [[2, 4, 7],[6, 2],[5, 6, 9]]
-        >>> bfrrergt = [[6, 4, 4],[4, 1],[0, 8, 4]]
+        >>> bfrh = [[1 , 2],[3, 4],[5, 6]]
+        >>> fru = [[1, 3], [1, 0], [1, 2]]
+        >>> bfrrergt = [[ 0, 0],[7, 5],[ 2, 1]]
         >>> tru = matrice(bfrrergt)
         >>> tr = matrice(bfrh)
+        >>> hg = matrice(fru)
         >>> tr.__str__()
         >>> tru.__str__()
+        >>> frhg = hg.add_mat(tru)
+        >>> frhg.__str__()
+        >>> nv = tr.transpose_mat()
+        >>> nv.__str__()
+
+        >>> mul1 = matrice([[1, 2, 3], [4, 5, 6]])
+        >>> mul2 = matrice([[7, 8], [9, -1], [-2, -3]])
+        >>> res = mul1.mul_mat(mul2)
+        >>> res.__str__()
         """
         self.taille = 0
         for i in range(len(mat)):
@@ -69,7 +80,25 @@ class matrice:
                 lig.append(self.__mat[j][i])
             mat.append(lig)
         return matrice(mat)
-        
+    
+
+    
+    def mul_mat(self, mat2):
+        if mat2.nb_lignes() != self.nb_colonne():
+            print("imposible")
+        else:
+            mat = []
+            for i in range(len(self.__mat)):
+                lig = []
+                for j in range(len(self.__mat)):
+                    somme = 0
+                    for k in range(len(self.__mat[0])):
+                        somme += self.__mat[i][k] * mat2.__mat[k][j]
+                    lig.append(somme)
+                mat.append(lig)
+            return matrice(mat)
+
+
 
 
 if __name__ == "__main__":

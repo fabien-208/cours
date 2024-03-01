8342603f18cef9c21e2ba982cd712491795a1b84
fabien <fabien.2084@gmail.com>
2024-03-01 11:52:49 +0100

    byb

--- cours/L1/Algorithme 2/TD/TD 6.py
+++ cours/L1/Algorithme 2/TD/TD 6.py
@@ -5,22 +5,22 @@ class caractere:
     def __init__(self, nom:str, pix=None, nb_lig:int = 8, nb_col:int = 16) -> None:
         """
         >>> car = caractere('A')
-        >>> car.valeur_pixel(0, 0)
+        >>> car.get_pixel(0, 0)
         0
         >>> car2 = caractere('Z')
-        >>> car.nb_pix_diff(car2)
+        >>> car.comparaison_pix(car2)
         0
         >>> car.carac_egaux(car2)
         True
         """
         self.__pixel = pix
         self.__nom = nom
-        self.nb_lig = nb_lig
-        self.nb_col = nb_col
+        self.__nb_lig = nb_lig
+        self.__nb_col = nb_col
         if self.__pixel ==None:
             self.__pixel = []
-            for i in range(self.nb_col):
-                for j in range(self.nb_lig):
+            for i in range(self.__nb_col):
+                for j in range(self.__nb_lig):
                     self.__pixel.append(0)
         
     def nom(self) -> str:
@@ -28,31 +28,31 @@ class caractere:
     
 # exercice 2
 
-    def valeur_pixel(self, l:int, c:int):
-        if l <= self.nb_lig:
-            return self.__pixel[(8*l + c)]
+    def get_pixel(self, l:int, c:int):
+        if l <= self.__nb_lig:
+            return self.__pixel[(self.__nb_lig*l + c)]
     
 # exercice 3
 
-    def nb_pix_diff(self, carac) ->int:
+    def comparaison_pix(self, carac) ->int:
         diff = 0
-        for i in range(self.nb_lig):
-            for j in range(self.nb_col):
-                if self.valeur_pixel(i*8, j) != carac.valeur_pixel(i*8, j):
+        for i in range(self.__nb_lig):
+            for j in range(self.__nb_col):
+                if self.get_pixel(i*self.__nb_lig, j) != carac.get_pixel(i*self.__nb_lig, j):
                     diff += 1
         return diff
     
 # exercice 4
 
     def carac_egaux(self, pixels)-> bool:
-        if self.nb_pix_diff(pixels) == 0:
+        if self.comparaison_pix(pixels) == 0:
             return True
         return False
     
 # exercice 5
 
     def carac_proche(self, pixels)-> bool:
-        if self.nb_pix_diff(pixels) > (5*(self.nb_lig*8+ self.nb_col))/100:
+        if self.comparaison_pix(pixels) < (5*(self.__nb_lig*self.__nb_lig+ self.__nb_col))/100:
             return True
         return False
 

1e7013144ef55120b6afd28b8be0acd3868ab905
fabien <fabien.2084@gmail.com>
2024-03-20 13:05:43 +0100

    bg

--- cours/L1/Algorithme 2/SAE/Flood_controleur.py
+++ cours/L1/Algorithme 2/SAE/Flood_controleur.py
@@ -1,2 +1,16 @@
-def cntrl():
-    pass
+import vue_flood
+import modele_flood
+
+class controle:
+
+    def __init__(self, vue= vue_flood.Vue(), modele =modele_flood.Modele()) -> None:
+        self.vue = vue
+        self.modele = modele
+
+    def quit(self):
+        self.vue.fenetre.destroy
+
+    def retry(self):
+        self.modele.__score = 0
+        self.modele.init_jeu()
+        self.vue.init_image()

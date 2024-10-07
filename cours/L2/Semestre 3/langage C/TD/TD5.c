f5b10567ecc14fcb3d1b248a8bf92ae8d84e19df
fabien_on_surface <fabien.2084@gmail.com>
2024-10-07 16:13:16 +0200

    TD langage C

--- cours/L2/Semestre 3/langage C/TD/TD5.c
+++ cours/L2/Semestre 3/langage C/TD/TD5.c
@@ -1,14 +1,30 @@
 # include <stdio.h>
 
-int nb_bit_a_1(int nb){
-    if nb == 0{
-        return 
+int nb_bit(int nb) {
+    if (nb == 0) {
+        return 0;
     }
+    else {
+        if (nb & 1 ){
+            return nb_bit_a_1(nb >> 1);
+        }
+        else{
+            return nb_bit_a_1(nb >> 1) + 1;
+        }
+    }
+}
+
+
+int nb_bit2(int nb){
+    if (nb == 0){
+        return 0;
+    }
+    return (nb % 2) + nb_2(nb/2);
 }
 
 
 
 int main(void){
-
+    nb_bit(14);
 }
 
